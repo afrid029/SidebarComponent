8920cd3416d027816349699f88700d6e62e224e0
Mohamed Afrid <mohamed.afrid@quadrabay.local>
2025-07-10 17:58:31 +0400

    Fixture01

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, Input, OnInit, ViewChild } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
 import { Ripple } from 'primeng/ripple';
@@ -9,21 +9,25 @@ import { Router } from '@angular/router';
 import { GetContentService } from '../../Services/get-content.service';
 import { SelectButton } from 'primeng/selectbutton';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ToggleSwitch, ToggleSwitchChangeEvent } from 'primeng/toggleswitch';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [AvatarModule, DrawerModule, ButtonModule, Ripple, StyleClass, SelectButton, FormsModule],
+  imports: [AvatarModule, DrawerModule, ButtonModule, Ripple, StyleClass, SelectButton, FormsModule, CommonModule, ToggleSwitch],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent implements OnInit {
   @ViewChild('drawerRef') drawerRef!: Drawer;
   @Input() visible: boolean = false;
+  @ViewChild('sideBar') sideBar!: ElementRef;
   options : any[] = [
     {label : "Admin" , value : "admin"},
     {label : "User" , value : "user"},
   ]
   role : string = 'admin'
+  checked : boolean = false;
 
   private _content : GetContentService = inject(GetContentService);
   private _router : Router = inject(Router);
@@ -44,9 +48,25 @@ openSlideBar() {
    this.loadSideBar()
     this.visible = true;
 }
+
+toggleFix($event : ToggleSwitchChangeEvent){
+    const toggleBtn = $event.originalEvent.currentTarget as HTMLElement;
+    const state = $event.checked;
+    // console.log(toggleBtn.classList);
+    
+    if(state) {
+        // toggleBtn.classList.add('')
+        this.sideBar.nativeElement.classList.add('fixed-bar')
+    } else {
+        this.sideBar.nativeElement.classList.remove('fixed-bar')
+    }
+    // console.log();
+    
+}
+
 toggleMain($event : Event) {
     const main = $event.currentTarget as HTMLElement;
-    const icon = main.childNodes[2] as HTMLElement;
+    const icon = main.childNodes[1] as HTMLElement;
     if(icon.classList.contains('slide-up')) {
         icon.classList.remove('slide-up');
         icon.classList.add('slide-down')
@@ -58,6 +78,8 @@ toggleMain($event : Event) {
 }
 toggleParent($event : Event) {
     const parent = $event.currentTarget as HTMLElement;
+    console.log(parent.classList);
+    
     
     if(parent.classList.contains('active-category')){
         parent.classList.remove('active-category');
--- src/app/Services/get-content.service.ts
+++ src/app/Services/get-content.service.ts
@@ -1,162 +1,170 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class GetContentService {
-  private AdminContents: any[] = [
+
+  private AdminContents : any [] = [
     {
-      icon: 'pi pi-star',
-      name: 'Favourites',
-      child: [
-        {
-          icon: 'pi pi-chart-bar',
-          name: 'Dashboard',
-          route: '/dashboard',
-        },
-        {
-          icon: 'pi pi-github',
-          name: 'Projects',
-          child: [
-            {
-              icon: 'pi pi-code',
-              name: 'Source code',
-              route: '---',
-            },
-            {
-              icon: 'pi pi-microchip',
-              name: 'Bussiness',
-              child: [
+    "icon" : "pi pi-star",
+    "name" : "Favourites",
+    "child" : [
+       {
+            "icon": "pi pi-chart-bar",
+            "name" : "Dashboard",
+            "route" : "/dashboard"
+       },
+       {
+            "icon" : "pi pi-github",
+            "name" : "Projects",
+            "child" : [
                 {
-                  icon: 'pi pi-amazon',
-                  name: 'Amazon',
-                  route: '/our-amazon',
+                    "icon" : "pi pi-code",
+                    "name" : "Source code",
+                    "route" : "---"
                 },
                 {
-                  icon: 'pi pi-google',
-                  name: 'Google',
-                  route: '/our-google',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          icon: 'pi pi-user',
-          name: 'Users',
-          route: '/user',
-        },
-      ],
-    },
+                    "icon" : "pi pi-microchip",
+                    "name" : "Bussiness",
+                    "child" : [
+                        {
+                            "icon" : "pi pi-amazon",
+                            "name" : "Amazon",
+                            "route" : "/our-amazon"
+                        },
+                        {
+                            "icon" : "pi pi-google",
+                            "name" : "Google",
+                            "route" : "/our-google"
+                        }
+                    ]
+                }
+            ]
+       },
+       {
+            "icon" : "pi pi-user",
+            "name" : "Users",
+            "route" : "/user"
+       }
+    ]
+},
+
 
     {
-      icon: 'pi pi-cog',
-      name: 'Settings',
-      route: '/setting',
-    },
+        "icon" : "pi pi-cog",
+        "name" : "Settings",
+        "route" : "/setting"
+    }, 
     {
-      icon: 'pi pi-shield',
-      name: 'Account & Privacy',
-      child: [
-        {
-          icon: 'pi pi-info-circle',
-          name: 'Personal Information',
-          route: '/personal-info',
-        },
-        {
-          icon: 'pi pi-wifi',
-          name: 'Contact Information',
-          route: '/contact',
-        },
-        {
-          icon: 'pi pi-bars',
-          name: 'Policies',
-          child: [
+        "icon" : "pi pi-shield",
+        "name" : "Account & Privacy",
+        "child" : [
             {
-              icon: 'pi pi-id-card',
-              name: 'Personal Policy',
-              route: '/personal-policy',
-            },
-            {
-              icon: 'pi pi-chart-line',
-              name: 'Group Policy',
-              route: '/group-policy',
-            },
-          ],
-        },
-      ],
-    },
-  ];
+                "icon" : "pi pi-info-circle",
+                "name" : "Personal Information",
+                "route" : "/personal-info",
 
-  private UserContents: any[] = [
-    {
-      icon: 'pi pi-star',
-      name: 'User Favourites',
-      child: [
-        {
-          icon: 'pi pi-github',
-          name: 'Projects',
-          child: [
-            {
-              icon: 'pi pi-code',
-              name: 'Source code',
-              route: '---',
             },
             {
-              icon: 'pi pi-microchip',
-              name: 'Bussiness',
-              child: [
+                "icon" : "pi pi-wifi",
+                "name" : "Contact Information",
+                "route" : "/contact"
+            },
+            {
+                "icon" : "pi pi-bars",
+                "name" : "Policies",
+                "child" : [
+                    {
+                        "icon" : "pi pi-id-card",
+                        "name" : "Personal Policy",
+                        "route" : "/personal-policy"
+                    },
+                    {
+                        "icon" : "pi pi-chart-line",
+                        "name" : "Group Policy",
+                        "route" : "/group-policy"
+                    }
+                ]
+            } 
+        ]
+    },
+
+    
+  ]
+
+  private UserContents : any [] = [
+    {
+    "icon" : "pi pi-star",
+    "name" : "User Favourites",
+    "child" : [
+       {
+            "icon" : "pi pi-github",
+            "name" : "Projects",
+            "child" : [
                 {
-                  icon: 'pi pi-amazon',
-                  name: 'Amazon',
-                  route: '/our-amazon',
+                    "icon" : "pi pi-code",
+                    "name" : "Source code",
+                    "route" : "---"
                 },
                 {
-                  icon: 'pi pi-google',
-                  name: 'Google',
-                  route: '/our-google',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
+                    "icon" : "pi pi-microchip",
+                    "name" : "Bussiness",
+                    "child" : [
+                        {
+                            "icon" : "pi pi-amazon",
+                            "name" : "Amazon",
+                            "route" : "/our-amazon"
+                        },
+                        {
+                            "icon" : "pi pi-google",
+                            "name" : "Google",
+                            "route" : "/our-google"
+                        }
+                    ]
+                }
+            ]
+       }
+    ]
+},
+
 
     {
-      icon: 'pi pi-shield',
-      name: 'Account & Privacy',
-      child: [
-        {
-          icon: 'pi pi-info-circle',
-          name: 'Personal Information',
-          route: '/personal-info',
-        },
-        {
-          icon: 'pi pi-wifi',
-          name: 'Contact Information',
-          route: '/contact',
-        },
-        {
-          icon: 'pi pi-bars',
-          name: 'Policies',
-          child: [
+        "icon" : "pi pi-shield",
+        "name" : "Account & Privacy",
+        "child" : [
             {
-              icon: 'pi pi-id-card',
-              name: 'Personal Policy',
-              route: '/personal-policy',
-            },
-          ],
-        },
-      ],
-    },
-  ];
+                "icon" : "pi pi-info-circle",
+                "name" : "Personal Information",
+                "route" : "/personal-info",
 
-  getSideBarContents(role: string) {
-    if (role == 'admin') {
-      return this.AdminContents;
+            },
+            {
+                "icon" : "pi pi-wifi",
+                "name" : "Contact Information",
+                "route" : "/contact"
+            },
+            {
+                "icon" : "pi pi-bars",
+                "name" : "Policies",
+                "child" : [
+                    {
+                        "icon" : "pi pi-id-card",
+                        "name" : "Personal Policy",
+                        "route" : "/personal-policy"
+                    }
+                ]
+            } 
+        ]
+    }
+  ]
+
+  getSideBarContents(role : string) {
+    if(role == 'admin') {
+      return this.AdminContents
     } else {
       return this.UserContents;
     }
   }
+
 }
